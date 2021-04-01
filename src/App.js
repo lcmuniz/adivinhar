b4e6ef71c5f1c7c07a8c10efcf7bdebb03e41b03
Luiz Carlos Muniz <lcmuniz@gmail.com>
2021-03-31 21:02:27 -0300

    primeira versao

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+
 import './App.css';
 
+let numero = Math.trunc(Math.random() * 100)
+
 function App() {
+
+  const [tentativas, setTentativas] = useState(0)
+  const [mensagem, setMensagem] = useState('')
+  const [acertou, setAcertou] = useState(false)
+
+  const palpiteInput = useRef()
+
+  function testarPalpite() {
+
+    const palpite = Number(palpiteInput.current.value)
+
+    if (palpite < numero) {
+      setMensagem('Seu palpite é muito pequeno!')
+    }
+    else if (palpite > numero) {
+      setMensagem('Seu palpite é muito grande!')
+    }
+    else {
+      setMensagem('Você acertou!')
+      setAcertou(true)
+    }
+
+    setTentativas(tentativas + 1)
+
+  }
+
+  function reiniciar() {
+    setMensagem('')
+    setAcertou(false)
+    setTentativas(0)
+    palpiteInput.current.value = ''
+    palpiteInput.current.focus()
+    numero = Math.trunc(Math.random() * 100)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="fonte-maior">Adivinhe o número em que estou pensando.</p>
+      
+      <div>
+        <input ref={palpiteInput} className="numero" type="text" />
+        <button className="enviar" onClick={testarPalpite}>Enviar</button>
+      </div>
+
+      <p className="fonte-maior">{mensagem}</p>
+
+      { (acertou === true) ? 
+        <button onClick={reiniciar}>Clique aqui para reiniciar</button> 
+        : null }
+      
+      <p>Número de tentantivas: {tentativas}</p>
     </div>
   );
 }
